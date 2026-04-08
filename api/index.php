ab91232e1d343df472ac950ef57fbbe6cd6f50a6
Ramim Mahabub <kmahin498@gmail.com>
2026-04-08 23:13:50 +0600

    Fix boot bypass syntax and missing data path

--- api/index.php
+++ api/index.php
@@ -14,6 +14,7 @@ $storagePath = '/tmp/storage';
 if (!is_dir($storagePath)) {
     mkdir($storagePath, 0777, true);
     mkdir($storagePath . '/framework/cache', 0777, true);
+    mkdir($storagePath . '/framework/cache/data', 0777, true);
     mkdir($storagePath . '/framework/sessions', 0777, true);
     mkdir($storagePath . '/framework/views', 0777, true);
     mkdir($storagePath . '/logs', 0777, true);
